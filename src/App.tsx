e27fc6a6513b9d2cce16ef3b013b2397ba7e2d05
Anne Martinez <martinezannemerlyn@gmail.com>
2025-03-14 11:47:26 +0100

    adding analytics

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import MainPage from './components/MainPage';
 import Footer from './components/Footer';
 import { ThemeProvider } from './contexts/ThemeContext';
 import { LanguageProvider } from './contexts/LanguageContext';
+import { Analytics } from '@vercel/analytics/react';
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
           <MainPage />
           <Footer />
         </div>
+        <Analytics />
       </LanguageProvider>
     </ThemeProvider>
   );
